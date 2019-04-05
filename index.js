221a550ee49d2f27a639b96f188036c1a740d73a
Kevin Dai <daikevin19@yahoo.com>
2019-04-05 02:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = new Object({keys:`0`})
 function updateObjectWithKeyAndValue(object,key,value) {
   return Object.assign({}, object, {key:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key]=value
+  return object
+}
