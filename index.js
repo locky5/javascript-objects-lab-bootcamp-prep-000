df322731e3488fad9145fcebf1e8cc612e85cfb5
Kevin Dai <daikevin19@yahoo.com>
2019-04-05 02:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function updateObjectWithKeyAndValue(object,key,value) {
   return Object.assign({}, object, {key:value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  object[key]=value
-  return object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object,key) {
